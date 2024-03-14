7973692537064793fc0a2326f2b3582dd3381c2c
Andreas Waldis <andreas.waldis@hslu.ch>
2024-03-14 18:57:17 +0100

    initial commit

--- explorer.py
+++ explorer.py
@@ -5,9 +5,6 @@ from scipy.stats import kendalltau
 
 from utils import read_data, get_rankings, aggregate_results, get_polar_plot
 
-if 'sidebar_state' not in st.session_state:
-    st.session_state.sidebar_state = 'expanded'
-
 st.set_page_config(layout="wide")
 st.title("Holmes Explorer")
 
